51f65a6bc212a4a95a6bbdbe758d7d42f1d01d21
Ankita mishra <ankita.mishra@tarento.com>
2019-08-28 17:07:55 +0530

    camera opening

---
+++ rn_facedetector/src/rncamera.js
@@ -0,0 +1,86 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Fragment,  Component }  from 'react';
+
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  TouchableOpacity,
+  View,
+  Text,
+  StatusBar,
+  
+} from 'react-native';
+import { RNCamera, FaceDetector } from 'react-native-camera';
+
+const takePicture = async function(camera) {
+  const options = { quality: 0.5, base64: true };
+  const data = await camera.takePictureAsync(options);
+  //  eslint-disable-next-line
+  console.log(data.uri)
+}
+
+class rncamera extends Component {
+ render() {
+  return (
+      <View style={styles.container}>
+              <RNCamera
+          style={styles.preview}
+          type={RNCamera.Constants.Type.front}
+          flashMode={RNCamera.Constants.FlashMode.on}
+          androidCameraPermissionOptions={{
+            title: 'Permission to use camera',
+            message: 'We need your permission to use your camera',
+            buttonPositive: 'Ok',
+            buttonNegative: 'Cancel',
+          }}
+          
+        >
+          {({ camera, status }) => {
+            //if (status !== 'READY') return <PendingView />;
+            return (
+              <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
+                <TouchableOpacity onPress={() => takePicture(camera)} style={styles.capture}>
+                  <Text style={{ fontSize: 14 }}> SNAP </Text>
+                </TouchableOpacity>
+              </View>
+            );
+          }}
+        </RNCamera>
+        </View>
+  )
+        }
+    }
+
+
+        const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: 'black',
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    padding: 15,
+    paddingHorizontal: 20,
+    alignSelf: 'center',
+    margin: 20,
+  },
+});
+
+
+export default rncamera;
