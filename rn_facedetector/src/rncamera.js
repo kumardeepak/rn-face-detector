910e7e040879d0774b12ffe412006ea9e80fd92e
Ankita mishra <ankita.mishra@tarento.com>
2019-09-17 12:38:56 +0530

    facedetector

--- rn_facedetector/src/rncamera.js
+++ rn_facedetector/src/rncamera.js
@@ -1,86 +1,425 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+// /**
+//  * Sample React Native App
+//  * https://github.com/facebook/react-native
+//  *
+//  * @format
+//  * @flow
+//  */
 
-import React, {Fragment,  Component }  from 'react';
+// import React, {Fragment,  Component }  from 'react';
 
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  TouchableOpacity,
-  View,
-  Text,
-  StatusBar,
+// import {
+//   SafeAreaView,
+//   StyleSheet,
+//   ScrollView,
+//   TouchableOpacity,
+//   View,
+//   Text,
+//   StatusBar,
   
-} from 'react-native';
-import { RNCamera, FaceDetector } from 'react-native-camera';
+// } from 'react-native';
+// import { RNCamera, FaceDetector } from 'react-native-camera';
 
-const takePicture = async function(camera) {
-  const options = { quality: 0.5, base64: true };
-  const data = await camera.takePictureAsync(options);
-  //  eslint-disable-next-line
-  console.log(data.uri)
+// const takePicture = async function(camera) {
+//   const options = { quality: 0.5, base64: true };
+//   const data = await camera.takePictureAsync(options);
+//   //  eslint-disable-next-line
+//   console.log(data.uri)
+// }
+
+// class rncamera extends Component {
+//  render() {
+//   return (
+//       <View style={styles.container}>
+//               <RNCamera
+//           style={styles.preview}
+//           type={RNCamera.Constants.Type.front}
+//           flashMode={RNCamera.Constants.FlashMode.on}
+//           androidCameraPermissionOptions={{
+//             title: 'Permission to use camera',
+//             message: 'We need your permission to use your camera',
+//             buttonPositive: 'Ok',
+//             buttonNegative: 'Cancel',
+//           }}
+          
+//         >
+//           {({ camera, status }) => {
+//             //if (status !== 'READY') return <PendingView />;
+//             return (
+//               <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
+//                 <TouchableOpacity onPress={() => takePicture(camera)} style={styles.capture}>
+//                   <Text style={{ fontSize: 14 }}> SNAP </Text>
+//                 </TouchableOpacity>
+//               </View>
+//             );
+//           }}
+//         </RNCamera>
+//         </View>
+//   )
+//         }
+//     }
+
+
+//         const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     flexDirection: 'column',
+//     backgroundColor: 'black',
+//   },
+//   preview: {
+//     flex: 1,
+//     justifyContent: 'flex-end',
+//     alignItems: 'center',
+//   },
+//   capture: {
+//     flex: 0,
+//     backgroundColor: '#fff',
+//     borderRadius: 5,
+//     padding: 15,
+//     paddingHorizontal: 20,
+//     alignSelf: 'center',
+//     margin: 20,
+//   },
+// });
+
+
+// export default rncamera;
+
+
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Slider } from 'react-native';
+
+import { RNCamera } from 'react-native-camera';
+
+
+
+const landmarkSize = 2;
+
+export default class rncamera extends React.Component {
+  state = {
+    flash: 'off',
+    zoom: 0,
+    autoFocus: 'on',
+    depth: 0,
+    type: 'front',
+    whiteBalance: 'auto',
+    ratio: '16:9',
+    recordOptions: {
+      mute: false,
+      maxDuration: 5,
+      quality: RNCamera.Constants.VideoQuality['288p'],
+    },
+    isRecording: false,
+    canDetectFaces: false,
+    canDetectText: false,
+    canDetectBarcode: false,
+    faces: [],
+    textBlocks: [],
+    barcodes: [],
+    yawn:0
+  };
+
+  toggleFacing() {
+    this.setState({
+      type: this.state.type === 'back' ? 'front' : 'back',
+    });
+  }
+
+
+
+  setFocusDepth(depth) {
+    this.setState({
+      depth,
+    });
+  }
+
+  takePicture = async function () {
+    if (this.camera) {
+      const data = await this.camera.takePictureAsync();
+      console.warn('takePicture ', data);
+    }
+  };
+
+
+
+  toggle = value => () => this.setState(prevState => ({ [value]: !prevState[value] }));
+
+  facesDetected = ({ faces }) => this.setState({ faces });
+
+  renderFace = ({ bounds, faceID, rollAngle, yawAngle }) => (
+    
+    <View 
+    style={[
+      styles.face,
+      {
+        ...bounds.size,
+        left: bounds.origin.x,
+        top: bounds.origin.y,
+      },
+    ]}>
+    <View
+      key={faceID}
+      transform={[
+        { perspective: 600 },
+        { rotateZ: `${rollAngle.toFixed(0)}deg` },
+        { rotateY: `${yawAngle.toFixed(0)}deg` },
+      ]}
+      // style={[
+      //   styles.face,
+      //   {
+      //     ...bounds.size,
+      //     left: bounds.origin.x,
+      //     top: bounds.origin.y,
+      //   },
+      // ]}
+    >
+      {/* <Text style={styles.faceText}>ID: {faceID}</Text>
+      <Text style={styles.faceText}>rollAngle: {rollAngle.toFixed(0)}</Text>
+      <Text style={styles.faceText}>yawAngle: {yawAngle.toFixed(0)}</Text> */}
+    </View>
+    <View  style= {{flexDirection:'row'}}>
+       <Text style={styles.faceText}>rollAngle: {rollAngle.toFixed(0)}</Text>
+      <Text style={styles.faceText}>yawAngle: {yawAngle.toFixed(0)}</Text>
+    </View>
+    </View>
+  );
+
+  renderLandmarksOfFace(face) {
+    const renderLandmark = position =>
+      position && (
+        <View
+          style={[
+            styles.landmark,
+            {
+              left: position.x - landmarkSize / 2,
+              top: position.y - landmarkSize / 2,
+            },
+          ]}
+        />
+      );
+    return (
+      <View key={`landmarks-${face.faceID}`}>
+        {renderLandmark(face.leftEyePosition)}
+        {renderLandmark(face.rightEyePosition)}
+        {renderLandmark(face.leftEarPosition)}
+        {renderLandmark(face.rightEarPosition)}
+        {renderLandmark(face.leftCheekPosition)}
+        {renderLandmark(face.rightCheekPosition)}
+        {renderLandmark(face.leftMouthPosition)}
+        {renderLandmark(face.mouthPosition)}
+        {renderLandmark(face.rightMouthPosition)}
+        {renderLandmark(face.noseBasePosition)}
+        {renderLandmark(face.bottomMouthPosition)}
+      </View>
+    );
+  }
+
+  renderFaces = () => (
+    <View style={styles.facesContainer} pointerEvents="none">
+      {this.state.faces.map(this.renderFace)}
+    </View>
+  );
+
+  renderLandmarks = () => (
+    <View style={styles.facesContainer} pointerEvents="none">
+      {this.state.faces.map(this.renderLandmarksOfFace)}
+    </View>
+  );
+
+
+
+  renderCamera() {
+    const { canDetectFaces, canDetectText, canDetectBarcode } = this.state;
+    return (
+      <RNCamera
+        ref={ref => {
+          this.camera = ref;
+        }}
+        style={{
+          flex: 1,
+        }}
+        type={this.state.type}
+        flashMode={this.state.flash}
+        autoFocus={this.state.autoFocus}
+        zoom={this.state.zoom}
+        whiteBalance={this.state.whiteBalance}
+        ratio={this.state.ratio}
+        focusDepth={this.state.depth}
+        trackingEnabled
+        androidCameraPermissionOptions={{
+          title: 'Permission to use camera',
+          message: 'We need your permission to use your camera',
+          buttonPositive: 'Ok',
+          buttonNegative: 'Cancel',
+        }}
+        faceDetectionLandmarks={
+          RNCamera.Constants.FaceDetection.Landmarks
+            ? RNCamera.Constants.FaceDetection.Landmarks.all
+            : undefined
+        }
+        faceDetectionClassifications={
+          RNCamera.Constants.FaceDetection.Classifications
+            ? RNCamera.Constants.FaceDetection.Classifications.all
+            : undefined
+        }
+        onFacesDetected={canDetectFaces ? this.facesDetected : null}
+        onTextRecognized={canDetectText ? this.textRecognized : null}
+        onGoogleVisionBarcodesDetected={canDetectBarcode ? this.barcodeRecognized : null}
+        googleVisionBarcodeType={RNCamera.Constants.GoogleVisionBarcodeDetection.BarcodeType.ALL}
+      >
+        <View
+          style={{
+            flex: 0.5,
+          }}
+        >
+          <View
+            style={{
+              backgroundColor: 'transparent',
+              flexDirection: 'row',
+              justifyContent: 'space-around',
+            }}
+          >
+            {/* <TouchableOpacity style={styles.flipButton} onPress={this.toggleFacing.bind(this)}>
+              <Text style={styles.flipText}> {this.state.yawn} </Text>
+            </TouchableOpacity> */}
+
+          </View>
+          <View
+            style={{
+              backgroundColor: 'transparent',
+              flexDirection: 'row',
+              justifyContent: 'space-around',
+            }}
+          >
+            <TouchableOpacity onPress={this.toggle('canDetectFaces')} style={styles.flipButton}>
+              <Text style={styles.flipText}>
+                {!canDetectFaces ? 'Detect Faces' : 'Detecting Faces'}
+              </Text>
+            </TouchableOpacity>
+
+          </View>
+        </View>
+        <View
+          style={{
+            flex: 0.4,
+            backgroundColor: 'transparent',
+            flexDirection: 'row',
+            alignSelf: 'flex-end',
+          }}
+        >
+          <Slider
+            style={{ width: 150, marginTop: 15, alignSelf: 'flex-end' }}
+            onValueChange={this.setFocusDepth.bind(this)}
+            step={0.1}
+            disabled={this.state.autoFocus === 'on'}
+          />
+        </View>
+        <View
+          style={{
+            flex: 0.1,
+            backgroundColor: 'transparent',
+            flexDirection: 'row',
+            alignSelf: 'flex-end',
+          }}
+        >
+
+
+
+
+
+          <TouchableOpacity
+            style={[styles.flipButton, styles.picButton, { flex: 0.3, alignSelf: 'flex-end' }]}
+            onPress={this.takePicture.bind(this)}
+          >
+            <Text style={styles.flipText}> SNAP </Text>
+          </TouchableOpacity>
+        </View>
+        {!!canDetectFaces && this.renderFaces()}
+        {!!canDetectFaces && this.renderLandmarks()}
+
+      </RNCamera>
+    );
+  }
+
+  render() {
+    return <View style={styles.container}>{this.renderCamera()}</View>;
+  }
 }
 
-class rncamera extends Component {
- render() {
-  return (
-      <View style={styles.container}>
-              <RNCamera
-          style={styles.preview}
-          type={RNCamera.Constants.Type.front}
-          flashMode={RNCamera.Constants.FlashMode.on}
-          androidCameraPermissionOptions={{
-            title: 'Permission to use camera',
-            message: 'We need your permission to use your camera',
-            buttonPositive: 'Ok',
-            buttonNegative: 'Cancel',
-          }}
-          
-        >
-          {({ camera, status }) => {
-            //if (status !== 'READY') return <PendingView />;
-            return (
-              <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
-                <TouchableOpacity onPress={() => takePicture(camera)} style={styles.capture}>
-                  <Text style={{ fontSize: 14 }}> SNAP </Text>
-                </TouchableOpacity>
-              </View>
-            );
-          }}
-        </RNCamera>
-        </View>
-  )
-        }
-    }
-
-
-        const styles = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'column',
-    backgroundColor: 'black',
+    paddingTop: 10,
+    backgroundColor: '#000',
   },
-  preview: {
-    flex: 1,
-    justifyContent: 'flex-end',
+  flipButton: {
+    flex: 0.3,
+    height: 40,
+    marginHorizontal: 2,
+    marginBottom: 10,
+    marginTop: 10,
+    borderRadius: 8,
+    borderColor: 'white',
+    borderWidth: 1,
+    padding: 5,
     alignItems: 'center',
+    justifyContent: 'center',
   },
-  capture: {
-    flex: 0,
-    backgroundColor: '#fff',
-    borderRadius: 5,
-    padding: 15,
-    paddingHorizontal: 20,
-    alignSelf: 'center',
-    margin: 20,
+  flipText: {
+    color: 'white',
+    fontSize: 15,
+  },
+  zoomText: {
+    position: 'absolute',
+    bottom: 70,
+    zIndex: 2,
+    left: 2,
+  },
+  picButton: {
+    backgroundColor: 'darkseagreen',
+  },
+  facesContainer: {
+    position: 'absolute',
+    bottom: 0,
+    right: 0,
+    left: 0,
+    top: 0,
+  },
+  face: {
+    padding: 10,
+    borderWidth: 2,
+    borderRadius: 2,
+    // position: 'absolute',
+    borderColor: '#FFD700',
+    justifyContent: 'flex-end',
+    backgroundColor: 'rgba(0, 0, 0, 0)',
+  },
+  landmark: {
+    width: landmarkSize,
+    height: landmarkSize,
+    position: 'absolute',
+    backgroundColor: 'red',
+  },
+  faceText: {
+    color: '#FFD700',
+    fontWeight: 'bold',
+    textAlign: 'center',
+    // margin: 10,
+    backgroundColor: 'transparent',
+  },
+  text: {
+    padding: 10,
+    borderWidth: 2,
+    borderRadius: 2,
+    position: 'absolute',
+    borderColor: '#F00',
+    justifyContent: 'center',
+  },
+  textBlock: {
+    color: '#F00',
+    position: 'absolute',
+    textAlign: 'center',
+    backgroundColor: 'transparent',
   },
 });
 
-
-export default rncamera;
